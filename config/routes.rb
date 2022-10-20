7f53c872b23cb952f281bad5277b37d7c0e860be
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-21 02:45:05 +0300

    delete test models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :tests
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
    post '/login', to: 'sessions#create'
