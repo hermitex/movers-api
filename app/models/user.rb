7d45de10f90211a339428689a36e94a82268fa52
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-23 13:16:22 +0300

    fix pg error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
-class User < ApplicationRecord
-  belongs_to :location
-  has_secure_password
+# class User < ApplicationRecord
+#   belongs_to :location
+#   has_secure_password
 
-  validates :full_name, presence: true, length: { minimum: 3}
-  validates :email, presence: true, uniqueness: true
-  validates :phone, presence: true, uniqueness: true
-  validates :type, presence: true
+#   validates :full_name, presence: true, length: { minimum: 3}
+#   validates :email, presence: true, uniqueness: true
+#   validates :phone, presence: true, uniqueness: true
+#   validates :type, presence: true
 
-end
+# end
