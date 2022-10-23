cb3839883179bd8150e814b01b05f6dac4f54e32
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-23 17:42:34 +0300

    reinstate user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,51 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    def index
+        users = User.all
+        render json: users, status: :ok
     end
-  end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
     end
-  end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+    def find_user
+        mover = Mover.find_by(id: params[:user_id])
+        render json: mover, status: :ok
     end
+
+    def update
+        user = User.find_by(id: params[:user_id])
+        user.update!(user_params)
+        render json: user, status: :accepted
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+     def user_params
+        params.permit(:fullname, :password, :password_confirmation, :email)
+     end
+
+     def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+     end
+
+     def render_not_found
+        render json: {error: "User not found!"}, status: :not_found
+     end
+
 
     # Only allow a list of trusted parameters through.
     def user_params
