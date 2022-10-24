a0729ce98d9c35baa8e4f33b4548ffc72c9c832f
+ <+>
2022-10-24 23:21:44 +0300

    adding prices and book logic

--- app/models/move_booking.rb
+++ app/models/move_booking.rb
@@ -1,4 +1,15 @@
 class MoveBooking < ApplicationRecord
   belongs_to :customer
   belongs_to :mover
+
+
+
+  def sub_total
+    sum = 0
+    movers.each do |mover|
+      sum += mover.total_price
+    end
+    sum
+  end
+
 end
