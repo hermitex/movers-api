107e5c7afb3ec5b560202c0b6205bac34a9f0577
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-26 22:06:22 +0300

    add device

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_144058) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_184651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_144058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_checklist_id"
+    t.integer "count"
     t.index ["inventory_checklist_id"], name: "index_bed_room_items_on_inventory_checklist_id"
   end
 
@@ -58,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_144058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_checklist_id"
+    t.integer "count"
     t.index ["inventory_checklist_id"], name: "index_dining_room_items_on_inventory_checklist_id"
   end
 
@@ -84,6 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_144058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_checklist_id"
+    t.integer "count"
     t.index ["inventory_checklist_id"], name: "index_kitchen_items_on_inventory_checklist_id"
   end
 
@@ -94,6 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_144058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_checklist_id"
+    t.integer "count"
     t.index ["inventory_checklist_id"], name: "index_living_room_items_on_inventory_checklist_id"
   end
 
