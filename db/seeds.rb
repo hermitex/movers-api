9b44066db0a6ac2c02a6c4ed3a9f6db180e21f2c
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-11-01 00:11:46 +0300

    add rates seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,211 @@ User.create!(
       "account_type": "mover",
       "location_id": 2,
       "email": 'janedoe@gmail.com'
+    },
+    {
+      "full_name": 'James Sonya',
+      "phone": '0797165748',
+      "password": 'jamessonya',
+      "avatar_url": '',
+      "type": 'Mover',
+      "account_type": "mover",
+      "location_id": 2,
+      "email": 'amessonya@gmail.com'
+    },
+    {
+      "full_name": 'Denis Maina',
+      "phone": '0797165749',
+      "password": 'denismaina',
+      "avatar_url": '',
+      "type": 'Mover',
+      "account_type": "mover",
+      "location_id": 2,
+      "email": 'denismaina@gmail.com'
+    }
+  ]
+)
+
+Rate.create!(
+  [
+    {
+      "item_name": "cabinet",
+      "category": "living room",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 350,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "sofa",
+      "category": "living room",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 350,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "table",
+      "category": "living room",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 50,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "bed",
+      "category": "bedroom",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 50,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "side table",
+      "category": "bedroom",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "dining chair",
+      "category": "dining room",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "dining table",
+      "category": "dining room",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "cooker",
+      "category": "kitchen",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "fridge",
+      "category": "kitchen",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "washing machine",
+      "category": "kitchen",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "microwave",
+      "category": "kitchen",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "office chair",
+      "category": "office",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "office table",
+      "category": "office",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "grill",
+      "category": "outdoor",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
+    },
+    {
+      "item_name": "boxes",
+      "category": "boxes",
+      "count":0,
+      "distance": 10,
+      "flat_price": 450,
+      "price_per_unit": 30,
+      "dicount": 0.5,
+      "start": 15,
+      "end": 40,
+      "user_id": 2
     }
   ]
 )
