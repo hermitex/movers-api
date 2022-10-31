7318fe3b15d0dcc51b702918956364069e22b255
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-10-31 23:04:38 +0300

    modify the rates table

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,7 +1,9 @@
 class Rate < ApplicationRecord
   belongs_to :user
-  validates :item,  presence: true, uniqueness: true, :on => :create
-  validates :distance,  presence: true, length: {minimum: 5}
+  validates :item_name,  presence: true, uniqueness: true, :on => :create
+  validates :category,  presence: true, inclusion: ["living room", "bedroom", "dining room", "kitchen", "office", "outdoor", "boxes"]
+  validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :distance,  presence: true
   validates :flat_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price_per_unit, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :dicount, presence: true, numericality: { greater_than_or_equal_to: 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_082550) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_193548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -81,6 +81,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_082550) do
     t.string "nickname"
   end
 
+  create_table "my_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_my_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_my_users_on_reset_password_token", unique: true
+  end
+
   create_table "notifications", force: :cascade do |t|
     t.text "message"
     t.bigint "user_id", null: false
@@ -101,7 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_082550) do
   end
 
   create_table "rates", force: :cascade do |t|
-    t.string "item"
+    t.string "item_name"
     t.string "distance"
     t.decimal "flat_price"
     t.decimal "price_per_unit"
@@ -111,6 +123,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_082550) do
     t.decimal "start"
     t.decimal "end"
     t.bigint "user_id"
+    t.string "category"
+    t.integer "count"
     t.index ["user_id"], name: "index_rates_on_user_id"
   end
 
@@ -144,6 +158,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_082550) do
     t.datetime "updated_at", null: false
     t.boolean "email_cofirmed", default: false
     t.string "confirm_token"
+    t.string "encrypted_password", limit: 128, default: "", null: false
+    t.string "password_salt"
+    t.string "authentication_token"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "reset_password_token"
+    t.string "remember_token"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["location_id"], name: "index_users_on_location_id"
   end
 
